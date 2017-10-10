b7f90f2c18bd8cdd74c6dde5a91fafbaae130d87
ErickGarciaC <e.gch.2895@gmail.com>
2017-10-10 14:17:16 -0500

    Corrección app.js

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-console.log('Hola Mundo!', 'Fuck yeah!');
-var nombre = "Erick";
-var nick = "erick_gd";
-console.log(nombre, 'es', nick);
-
 const express = require('express')
 const app = express()
 
 var port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
