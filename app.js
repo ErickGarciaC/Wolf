cdd68eaddf2acc3db83752f7397296c462120371
ErickGarciaC <e.gch.2895@gmail.com>
2017-10-10 14:07:58 -0500

    Se modificó la forma de acceso al puerto

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ console.log(nombre, 'es', nick);
 const express = require('express')
 const app = express()
 
+var port = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
